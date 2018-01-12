9346f8e70ed611ce58b27bd2fdbaf5451600d172
yuanhaoyu <yhy929768691@gmail.com>
2018-01-12 22:09:58 +0800

    add params

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.listen(3000, () => {
 
 // router-set
 
-router.get('/imgtest', async (ctx, next) => {
-    await axios.get('https://image.baidu.com/channel/listjson?pn=0&rn=10&tag1=%E7%BE%8E%E5%A5%B3&tag2=%E5%85%A8%E9%83%A8&ie=utf8')
+router.get('/imgtest/:start/:size', async (ctx, next) => {
+    await axios.get(`https://image.baidu.com/channel/listjson?pn=${ctx.params.start}&rn=${ctx.params.size}&tag1=%E7%BE%8E%E5%A5%B3&tag2=%E5%85%A8%E9%83%A8&ie=utf8`)
     .then(res => {
         ctx.body = res.data;
     })
